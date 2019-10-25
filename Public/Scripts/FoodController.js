ab18bbcfe98759bb06aa651ba7c1125decf832d1
Luke C <badookey@Hotmail.com>
2019-10-25 17:00:57 +1100

    Created event to spawn oranges after a delay

--- Public/Scripts/FoodController.js
+++ Public/Scripts/FoodController.js
@@ -12,6 +12,11 @@ function spawnOranges() {
     var mySceneObject = createObjectFromPrefab();
     mySceneObject.getTransform().setWorldPosition(script.getTransform().getWorldPosition());
 }
+
+var delayedEvent = script.createEvent("DelayedCallbackEvent");
+delayedEvent.bind(spawnOranges);
+delayedEvent.reset(2);
+
 function createObjectFromPrefab() {
     if (script.orangeContainer) {
         var instanceObject = script.orangeContainer.instantiate(script.getSceneObject());
