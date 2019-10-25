26b06f2a8fae1b83b3b62540a742e690d3120de5
Luke C <badookey@Hotmail.com>
2019-10-25 16:51:30 +1100

    Made Food Controller trigger orange spawn on brow raise

--- Public/Scripts/FoodController.js
+++ Public/Scripts/FoodController.js
@@ -1,23 +1,16 @@
 // -----JS CODE-----
 // @input Asset.ObjectPrefab orangeContainer
 // @input Component.Camera camera
-script.createEvent("TouchStartEvent").bind(onTouchStart);
-function onTouchStart(e) {
-    print("Screen Touched");
-    if (script.camera) {
-        var touchPosition = e.getTouchPosition();
-        var worldPosition = script.camera.screenSpaceToWorldSpace(touchPosition, 0);
-        var mySceneObject = createObjectFromPrefab();
-        mySceneObject.getTransform().setWorldPosition(worldPosition);
-    }
-}
 script.createEvent("BrowsRaisedEvent").bind(onBrowRaise);
 function onBrowRaise(e) {
     if (script) {
-        var mySceneObject = createObjectFromPrefab();
-        mySceneObject.getTransform().setWorldPosition(script.getTransform().getWorldPosition());
+        spawnOranges();
     }
     
+}
+function spawnOranges() {
+    var mySceneObject = createObjectFromPrefab();
+    mySceneObject.getTransform().setWorldPosition(script.getTransform().getWorldPosition());
 }
 function createObjectFromPrefab() {
     if (script.orangeContainer) {
