fea4abf0213cf483e387f8e46876bb32b89a500a
Luke C <badookey@Hotmail.com>
2019-10-29 13:29:52 +1100

    Endgame - remove all onscreen oranges

--- Public/Scripts/FoodController.js
+++ Public/Scripts/FoodController.js
@@ -2,9 +2,11 @@
 // @input Asset.ObjectPrefab orangeContainer
 // @input Component.Camera camera
 // @input int numOrangesShouldSpawn
+// @input Component.ScriptComponent highScoreController
 script.createEvent("BrowsRaisedEvent").bind(onBrowRaise);
 
 var numOrangesSpawned = 0;
+var objectsSpawned = [];
 function onBrowRaise(e) {
     if (script) {
         spawnOranges();
@@ -15,16 +17,25 @@ function spawnOranges() {
     if (numOrangesSpawned < script.numOrangesShouldSpawn) {
         var mySceneObject = createObjectFromPrefab();
         mySceneObject.getTransform().setWorldPosition(script.getTransform().getWorldPosition());
+        objectsSpawned.push(mySceneObject);
+
         var delayedEvent = script.createEvent("DelayedCallbackEvent");
         delayedEvent.bind(spawnOranges);
         delayedEvent.reset(3.5);
         numOrangesSpawned++;
     }
+    else { //Spawned max amount of oranges -- the game is over
+        removeAllSpawnedObjects();
+        script.highScoreController.api.endGame();
+    }
     
 }
 
-spawnOranges();
-
+function removeAllSpawnedObjects() {
+    objectsSpawned.forEach(function (orangeContainer) {
+        orangeContainer.destroy();
+    });
+}
 
 function createObjectFromPrefab() {
     if (script.orangeContainer) {
@@ -35,3 +46,6 @@ function createObjectFromPrefab() {
         return undefined;
     }
 }
+
+//Starts the game initially
+spawnOranges();
